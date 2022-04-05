1006deca32c5c39bd91837d40ba3c44fbb31f7fe
Quacoo <quacoo.boat@gmail.com>
2022-04-05 00:03:25 +0000

    add movable boxes: recursive algorithm that allow player to push multiple boxes until the last box touches a wall

--- JS/Render.js
+++ JS/Render.js
@@ -1,5 +1,6 @@
 import OPTIONS from './Options.js'
-export const renderRoom = ( { canvasContext, roomSeed, player } ) =>{
+
+export const renderRoom = ( { canvasContext, roomSeed, player, boxes } ) =>{
 
   let indexShift = 0;
   let j = 0;
@@ -14,12 +15,16 @@ export const renderRoom = ( { canvasContext, roomSeed, player } ) =>{
           canvasContext.fillStyle = 'green';
           canvasContext.fillRect( (i - indexShift) * OPTIONS.blockSize, j * OPTIONS.blockSize, OPTIONS.blockSize, OPTIONS.blockSize);
         }
-        break;
+        break
         case 'p': {
           player.x = (i - indexShift);
           player.y = j;
         }
-        break;
+        break
+        case 'b': {
+          boxes[i] = 1
+        }
+        break
       }
     }
 }
@@ -30,6 +35,17 @@ export const renderPlayer =  ( { canvasContext, player } ) => {
   canvasContext.fillStyle = 'blue';
   canvasContext.fillRect( player.x * OPTIONS.blockSize, player.y * OPTIONS.blockSize, OPTIONS.blockSize, OPTIONS.blockSize);
 }
+
+export const renderBox = ({ canvasContext, index }) => {
+  canvasContext.fillStyle = 'yellow';
+  canvasContext.fillRect(  ( index % OPTIONS.roomSize ) * OPTIONS.blockSize, Math.floor(index/ OPTIONS.roomSize) * OPTIONS.blockSize, OPTIONS.blockSize, OPTIONS.blockSize);
+}
+
+
+export const removeBox = ({ canvasContext, index }) => {
+  canvasContext.clearRect(  ( index % OPTIONS.roomSize ) * OPTIONS.blockSize, Math.floor(index/ OPTIONS.roomSize) * OPTIONS.blockSize, OPTIONS.blockSize, OPTIONS.blockSize);
+}
+
 
 export const removePlayer =  ( { canvasContext, player } ) => {
   canvasContext.clearRect(player.x * OPTIONS.blockSize, player.y * OPTIONS.blockSize, OPTIONS.blockSize, OPTIONS.blockSize);
--- main.js
+++ main.js
@@ -1,48 +1,114 @@
-import { renderRoom, renderPlayer, removePlayer } from './JS/Render.js'
+import { renderRoom, renderPlayer,renderBox, removePlayer, removeBox } from './JS/Render.js'
 import OPTIONS from './JS/Options.js'
 
 const canvas = document.getElementById('canvas')
-const ctx = canvas.getContext('2d')
+const canvasContext = canvas.getContext('2d')
+
+let roomSeedBuffer = ""
+roomSeedBuffer += "**********"
+roomSeedBuffer += "*p  *    *"
+roomSeedBuffer += "*   bb   *"
+roomSeedBuffer += "*        *"
+roomSeedBuffer += "*    b   *"
+roomSeedBuffer += "*        *"
+roomSeedBuffer += "*        *"
+roomSeedBuffer += "*        *"
+roomSeedBuffer += "*        *"
+roomSeedBuffer += "**********"
+
+const boxes = Array(roomSeedBuffer.length).fill(0)
+
+boxes.render = function(){
+  for(let index = 0; index < this.length; index++){
+    if (this[index]) renderBox({ canvasContext , index});
+  }
+}
+
+boxes.move = function({ index, axis, direction }){
+  
+  const indexOfRow = Math.floor( index / OPTIONS.roomSize)
+  const indexOfColumn = index % OPTIONS.roomSize
+
+  let directionX = (axis === 'x') ? direction : 0
+  let directionY = (axis === 'y') ? direction : 0
+
+
+  let posX = indexOfColumn + directionX
+  let posY = (indexOfRow + directionY) 
+  
+  let nextIndex = posX + ( posY * OPTIONS.roomSize)
+  // console.log('index: ', index, 'next: ', nextIndex )
+
+  console.log(this[nextIndex])
+
+  let movable = true
+  
+  if( this[ nextIndex ] ) movable = this.move({ index: nextIndex, axis, direction })
+  else if(roomSeedBuffer[nextIndex] === '*') movable = false
+  
+  if ( movable ){
+  
+
+    this[ index ] = 0
+    removeBox({ canvasContext , index })
+    this[ nextIndex ] = 1
+    renderBox({ canvasContext , index: nextIndex});
+
+    return true
+
+  } else return false
+
+}
 
 const player = {
   x:0,
   y:0,
 }
 
-let roomSeedBuffer = ""
-roomSeedBuffer += "**********"
-roomSeedBuffer += "*p  *    *"
-roomSeedBuffer += "*   b    *"
-roomSeedBuffer += "*        *"
-roomSeedBuffer += "*        *"
-roomSeedBuffer += "*        *"
-roomSeedBuffer += "*        *"
-roomSeedBuffer += "*        *"
-roomSeedBuffer += "*        *"
-roomSeedBuffer += "**********"
+player.move = function( { axis , direction } ){
+  /*
+    axis can either be x or y
+    direction -1 or 1
+  */
+
+  let directionX = (axis === 'x') ? direction : 0
+  let directionY = (axis === 'y') ? direction : 0
+
+
+  let posY = OPTIONS.roomSize * ( this.y + directionY )
+  let posX = this.x + directionX
+  let nextIndex = posY + posX
+  let movable = true
+
+  if (boxes[nextIndex]) {
+    movable = boxes.move({index: nextIndex, axis, direction})
+  }
+  else if(roomSeedBuffer[ posY + posX ] === '*') movable = false
+
+  if(movable){
+    removePlayer({ canvasContext, player: this })
+    this[axis] += direction
+    renderPlayer({ canvasContext, player: this })
+  }
+
+}
 
 
 
 const controls = (event) => {
-  removePlayer({ canvasContext: ctx, player })
-
-
-
-  if(event.code === "ArrowUp" && roomSeedBuffer[ OPTIONS.roomSize * ( player.y - 1 ) + player.x ] !== '*') player.y -= 1
-  if(event.code === "ArrowDown" && roomSeedBuffer[ OPTIONS.roomSize * ( player.y + 1 ) + player.x ] !== '*') player.y += 1
-  if(event.code === "ArrowLeft" && roomSeedBuffer[ OPTIONS.roomSize * player.y  + ( player.x - 1 )  ] !== '*') player.x -= 1
-  if(event.code === "ArrowRight" && roomSeedBuffer[ OPTIONS.roomSize * player.y  + ( player.x + 1 )  ] !== '*') player.x += 1
-
-  renderPlayer({ canvasContext: ctx ,player })
+  if(event.code === "ArrowUp") player.move({axis:'y', direction: -1})
+  if(event.code === "ArrowDown") player.move({axis:'y', direction: 1})
+  if(event.code === "ArrowLeft") player.move({axis:'x', direction: -1})
+  if(event.code === "ArrowRight") player.move({axis:'x', direction: 1})
 }
 
-
 const draw = () => {
-  ctx.canvas.height = OPTIONS.blockSize * OPTIONS.roomSize;
-  ctx.canvas.width = OPTIONS.blockSize * OPTIONS.roomSize;
+  canvasContext.canvas.height = OPTIONS.blockSize * OPTIONS.roomSize;
+  canvasContext.canvas.width = OPTIONS.blockSize * OPTIONS.roomSize;
 
-  renderRoom( { canvasContext: ctx, roomSeed:roomSeedBuffer, player } )
-  renderPlayer({ canvasContext: ctx ,player })
+  renderRoom( { canvasContext, roomSeed:roomSeedBuffer, player, boxes } )
+  renderPlayer({ canvasContext ,player })
+  boxes.render()
 
   document.addEventListener('keyup', controls)
 }
